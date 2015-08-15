46e1656e978293796992adcac318c481b4e8077e
Sean Nelson <sean.nelson.95@outlook.com>
2015-08-15 12:43:36 +1000

    Added XML documentation for the Spell class.

--- SwinWartsSchoolOfMagic/Spell.cs
+++ SwinWartsSchoolOfMagic/Spell.cs
@@ -2,12 +2,21 @@
 
 namespace SwinWartsSchoolOfMagic
 {
+	/// <summary>
+	/// Spells can be cast in order to munipulate the
+	/// caster's surroundings or themselves. 
+	/// There are a few different types of spells, each
+	/// with their own effects.
+	/// </summary>
 	public class Spell
 	{
 		private string _name;
 		private SpellKind _kind;
 
-		//Properties
+		/// <summary>
+		/// The Name property represents the spell's name.
+		/// </summary>
+		/// <value>The Name property gets/sets the value of the string field, _name.</value>
 		public string Name
 		{
 			get 
@@ -20,6 +29,11 @@ namespace SwinWartsSchoolOfMagic
 			}
 		}
 
+		/// <summary>
+		/// The Kind property represents the spell's kind.
+		/// One of three kinds: Teleport, Heal or Invisibilty.
+		/// </summary>
+		/// <value>The Kind property gets the value of the SpellKind field, _kind.</value>
 		public SpellKind Kind
 		{
 			get 
@@ -28,14 +42,23 @@ namespace SwinWartsSchoolOfMagic
 			}
 		}
 
-		//Constructor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SwinWartsSchoolOfMagic.Spell"/> class.
+		/// </summary>
+		/// <param name="name">Used to indicate the spell's Name.</param>
+		/// <param name="kind">Used to indicate the spell's Kind.</param>
 		public Spell (string name, SpellKind kind)
 		{
 			_name = name;
 			_kind = kind;
 		}
 
-		//Methods
+		/// <summary>
+		/// Casts the instance of Spell and returns a message to the caster. 
+		/// </summary>
+		/// <returns>
+		/// Returns a string.
+		/// </returns>
 		public string Cast()
 		{
 
